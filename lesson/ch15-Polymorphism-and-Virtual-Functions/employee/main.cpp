9c84927b040953e0518f0e8cb1b86ecab2d83c66
Emirkan Burak Yılmaz <ebylmz17@gmail.com>
2021-12-07 23:30:36 +0300

    Employee Class

--- lesson/ch15-Polymorphism-and-Virtual-Functions/employee/main.cpp
+++ lesson/ch15-Polymorphism-and-Virtual-Functions/employee/main.cpp
@@ -3,43 +3,61 @@ using std::cout;
 using std::endl;
 using std::fixed;
 
+#include <vector>
+using std::vector;
+
 #include <iomanip>
 using std::setprecision;
 
-#include "commissionEmployee.h"
-using EmployeeGTU::CommissionEmployee;
+#include <typeinfo>
 
+#include "employee.h"
+using GTUEmployee::Employee;
+#include "hourlyEmployee.h"
+using GTUEmployee::HourlyEmployee;
+#include "salariedEmployee.h"
+using GTUEmployee::SalariedEmployee;
+#include "commissionEmployee.h"
+using GTUEmployee::CommissionEmployee;
 #include "basePlusCommissionEmployee.h"
-using EmployeeGTU::BasePlusCommissionEmployee;
+using GTUEmployee::BasePlusCommissionEmployee;
+
+void printViaReference (const Employee & e);
+void printViaPointer (const Employee * e);
 
 int main () {
-    CommissionEmployee commissionE("Sue", "Jones", "555-55-5555", 100, .06);    
-    BasePlusCommissionEmployee basePlusCommissionE("Bob", "Lewis", "333-33-3333", 5000, .04, 300 );
-    
-    // set floating-point output formatting
-    cout << fixed << setprecision(2);
-    
-    cout << "Print base-class and derived-class objects:\n\n";
-    commissionE.print(); // invokes base-class print
-    cout << "\n\n";
-    basePlusCommissionE.print(); // invokes derived-class print
-    
-    CommissionEmployee * ptr_commissionE = &commissionE;
-    cout << "\n\n\nCalling print with base-class pointer to "
-         << "\nbase-class object invokes base-class print function:\n\n";
-    ptr_commissionE->print(); // invokes base-class
-    
-    BasePlusCommissionEmployee * ptr_basePlusCommissionE = &basePlusCommissionE;
-    cout << "\n\n\nCalling print with derived-class pointer to "
-         << "\nderived-class object invokes derived-class "
-         << "print function:\n\n";
-    ptr_basePlusCommissionE->print(); // invokes derived-class print    
-    
-    ptr_commissionE = &basePlusCommissionE;
-    cout << "\n\n\nCalling print with base-class pointer to "
-         << "derived-class object\ninvokes base-class print "
-         << "function on that derived-class object:\n\n";
-    ptr_commissionE->print(); // invokes base-class print !!!
-    cout << endl;
-    return 0;
+     // set floating-point output formatting
+     cout << fixed << setprecision(2);
+
+     vector<Employee *> employees(4);
+     employees[0] = new SalariedEmployee("John", "Smith", "111-11-1111", 800);
+     employees[1] = new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75, 40);
+     employees[2] = new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000, .06);
+     employees[3] = new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000, .04, 300);
+
+     for (size_t i = 0; i < employees.size(); ++i) {
+          employees[i]->print();
+          cout << endl;
+
+          // downcast pointer
+          BasePlusCommissionEmployee * derivedPtr = 
+               dynamic_cast<BasePlusCommissionEmployee *>(employees[i]);
+          if (derivedPtr != nullptr) {
+               double oldBaseSalary = derivedPtr->getBaseSalary();
+               cout << "old base salary: $" << oldBaseSalary << endl;
+               derivedPtr->setBaseSalary( 1.10 * oldBaseSalary );
+               cout << "new base salary with 10% increase is: $" << derivedPtr->getBaseSalary() << endl;
+          }
+          cout << "earned $" << employees[i]->earnings() << "\n\n";
+     }
+
+     for (size_t i = 0; i < employees.size(); ++i) {
+          // typeid::name
+          cout << "deleting object of " << typeid(*employees[i]).name() << endl;
+
+          delete employees[i];
+     }
+
+     cout << endl;
+     return 0;
 } 
